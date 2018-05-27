0b64e025a6aa50dee3defc52dc453dc34e5047e7
youyongpeng <youyongpeng0613@qq.com>
2018-05-27 13:39:18 +0800

    提交

--- src/main/java/com/yyp/WebPermissionApplication.java
+++ src/main/java/com/yyp/WebPermissionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebPermissionApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WebPermissionApplication.class, args);
 	}
 }
